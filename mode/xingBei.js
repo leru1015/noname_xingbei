995efb672db8837394bef352c27e2291f8d7434b
NongJie <245722665@qq.com>
2025-01-21 22:49:56 +0800

    复制回合函数到星杯文件中

--- mode/xingBei.js
+++ mode/xingBei.js
@@ -3202,6 +3202,200 @@ export default () => {
 		},
 		element:{
 			content:{
+				phase: function () {
+					"step 0";
+					//规则集中的“回合开始后③（处理“游戏开始时”的时机）”
+					//提前phaseBefore时机解决“游戏开始时”时机和“一轮开始时”先后
+					event.trigger("phaseBefore");
+					"step 1";
+					game.phaseNumber++;
+					//初始化阶段列表
+					if (!event.phaseList) {
+						event.phaseList = [ "phaseUse"];
+					}
+					if (typeof event.num != "number") {
+						event.num = 0;
+					}
+					//规则集中的“回合开始后①”，更新游戏轮数，触发“一轮游戏开始时”
+					var isRound = false;
+					if (lib.onround.every(i => i(event, player))) {
+						isRound = _status.roundSkipped;
+						if (_status.isRoundFilter) {
+							isRound = _status.isRoundFilter(event, player);
+						} else if (_status.seatNumSettled) {
+							var seatNum = player.getSeatNum();
+							if (seatNum != 0) {
+								if (get.itemtype(_status.lastPhasedPlayer) != "player" || seatNum < _status.lastPhasedPlayer.getSeatNum()) isRound = true;
+								_status.lastPhasedPlayer = player;
+							}
+						} else if (player == _status.roundStart) isRound = true;
+						if (isRound) {
+							delete _status.roundSkipped;
+							game.roundNumber++;
+							event._roundStart = true;
+							game.updateRoundNumber();
+							for (var i = 0; i < game.players.length; i++) {
+								if (game.players[i].isOut() && game.players[i].outCount > 0) {
+									game.players[i].outCount--;
+									if (game.players[i].outCount == 0 && !game.players[i].outSkills) {
+										game.players[i].in();
+									}
+								}
+							}
+							event.trigger("roundStart");
+						}
+					}
+					_status.globalHistory.push({
+						cardMove: [],
+						custom: [],
+						useCard: [],
+						changeHp: [],
+						everything: [],
+					});
+			
+					var players = game.players.slice(0).concat(game.dead);
+					for (var i = 0; i < players.length; i++) {
+						var current = players[i];
+						current.actionHistory.push({
+							useCard: [],
+							respond: [],
+							skipped: [],
+							lose: [],
+							gain: [],
+							sourceDamage: [],
+							damage: [],
+							custom: [],
+							useSkill: [],
+						});
+						current.stat.push({ card: {}, skill: {} });
+						if (isRound) {
+							current.getHistory().isRound = true;
+							current.getStat().isRound = true;
+						}
+					}
+					if (isRound) {
+						game.getGlobalHistory().isRound = true;
+					}
+					"step 2";
+					//规则集中的“回合开始后②（1v1武将登场专用）”
+					event.trigger("phaseBeforeStart");
+					"step 3";
+					//规则集中的“回合开始后④（卑弥呼〖纵傀〗的时机）”
+					event.trigger("phaseBeforeEnd");
+					"step 4";
+					//规则集中的“回合开始后⑤”，进行翻面检测
+					if (player.isTurnedOver() && !event._noTurnOver) {
+						player.turnOver();
+						player.phaseSkipped = true;
+						var players = game.players.slice(0).concat(game.dead);
+						for (var i = 0; i < players.length; i++) {
+							var current = players[i];
+							current.getHistory().isSkipped = true;
+							current.getStat().isSkipped = true;
+						}
+						event.cancel();
+					} else {
+						player.phaseSkipped = false;
+						player.getHistory().isMe = true;
+						player.getStat().isMe = true;
+					}
+					"step 5";
+					//规则集中的“回合开始后⑥”，更新“当前回合角色”
+					while (ui.dialogs.length) {
+						ui.dialogs[0].close();
+					}
+					player.phaseNumber++;
+					game.broadcastAll(
+						function (player, num, popup) {
+							if (lib.config.glow_phase) {
+								player.classList.add("glow_phase");
+							}
+							player.phaseNumber = num;
+							_status.currentPhase = player;
+							if (popup && lib.config.show_phase_prompt) player.popup("回合开始", null, false);
+						},
+						player,
+						player.phaseNumber,
+						!player.noPhaseDelay
+					);
+					_status.currentPhase = player;
+					_status.discarded = [];
+					game.syncState();
+					game.addVideo("phaseChange", player);
+					if (game.phaseNumber == 1) {
+						if (lib.configOL.observe) {
+							lib.configOL.observeReady = true;
+							game.send("server", "config", lib.configOL);
+						}
+					}
+					game.log('————————————————————');
+					game.log(player, "的回合开始");
+					player._noVibrate = true;
+					player.ai.tempIgnore = [];
+					if (ui.land && ui.land.player == player) {
+						game.addVideo("destroyLand");
+						ui.land.destroy();
+					}
+					"step 6";
+					//规则集中的“回合开始后⑦”，国战武将明置武将牌
+					event.trigger("phaseBeginStart");
+					"step 7";
+					//规则集中的“回合开始后⑨”，进行当先，化身等操作
+					//没有⑧ 因为⑧用不到
+					event.trigger("phaseBegin");
+					//阶段部分
+					"step 8";
+					if (num < event.phaseList.length) {
+						//规则集中没有的新时机 可以用来插入额外阶段啥的
+						if (player.isIn()) event.trigger("phaseChange");
+					} else event.goto(11);
+					"step 9";
+					if (player.isIn() && num < event.phaseList.length) {
+						var phase = event.phaseList[num].split("|");
+						event.currentPhase = phase[0];
+						var next = player[event.currentPhase]();
+						next.phaseIndex = num;
+						if (phase.length > 1) {
+							next._extraPhaseReason = phase[1];
+						}
+						if (event.currentPhase == "phaseDraw" || event.currentPhase == "phaseDiscard") {
+							if (!player.noPhaseDelay) {
+								if (player == game.me) {
+									game.delay();
+								} else {
+									game.delayx();
+								}
+							}
+						}
+					}
+					"step 10";
+					if (event.currentPhase == "phaseUse") {
+						game.broadcastAll(function () {
+							if (ui.tempnowuxie) {
+								ui.tempnowuxie.close();
+								delete ui.tempnowuxie;
+							}
+						});
+						delete player._noSkill;
+					}
+					event.num++;
+					"step 11";
+					if (event.num < event.phaseList.length) {
+						event.goto(8);
+					} else if (!event._phaseEndTriggered) {
+						event._phaseEndTriggered = true;
+						event.trigger("phaseEnd");
+						event.redo();
+					}
+					"step 12";
+					event.trigger("phaseAfter");
+					"step 13";
+					//删除当前回合角色 此时处于“不属于任何角色的回合”的阶段
+					game.broadcastAll(function (player) {
+						player.classList.remove("glow_phase");
+						delete _status.currentPhase;
+					}, player);
+				},
 				phaseUse:function(){
 					"step 0";
 					//xingBei设置
