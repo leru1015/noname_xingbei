a1c6a92db7e60e69d3f0c6c5f9b851d648989704
NongJie <245722665@qq.com>
2025-02-22 12:48:54 +0800

    优化英灵召唤描述，增加句号

--- character/shiZhouNian.js
+++ character/shiZhouNian.js
@@ -8964,7 +8964,7 @@ game.import('character',function(lib,game,ui,get,ai,_status){
             junShenWeiGuan:"[被动]军神威光",
             junShenWeiGuan_info:"<span class='tiaoJian'>(回合开始时，若你处于【英灵形态】)</span>选择以下1项发动：<br>·你+1[治疗]，[重置]脱离【英灵形态】；<br>·<span class='tiaoJian'>(移除我方【战绩区】X个星石，X<3)</span>目标角色+X[治疗]。",
             yingLingZhaoHuan:"[响应]英灵召唤",
-            yingLingZhaoHuan_info:"[水晶]<span class='tiaoJian'>(攻击命中时发动②)</span>本次攻击伤害额外+1，<span class='tiaoJian'>(若你额外弃置1张法术牌[展示])</span>目标角色+1[治疗]",   
+            yingLingZhaoHuan_info:"[水晶]<span class='tiaoJian'>(攻击命中时发动②)</span>本次攻击伤害额外+1，<span class='tiaoJian'>(若你额外弃置1张法术牌[展示])</span>目标角色+1[治疗]。",   
 
             //元素师
             yuanSuXiShou:'[响应]元素吸收',
