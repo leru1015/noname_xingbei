960dde5ec72b3049457e5c35f645c038652b69e9
NongJie <245722665@qq.com>
2025-03-07 19:16:43 +0800

    更新1.0.1

--- game/update.js
+++ game/update.js
@@ -1,8 +1,8 @@
 window.noname_update = {
-	version: "1.0.0",
-	update: "1.0.0", //新版本更新文件较多，直接强制进行全量更新
+	version: "1.0.1",
+	update: "1.0.1", //新版本更新文件较多，直接强制进行全量更新
 	changeLog: [
-		
+		'优化文件及函数文件架构'
 	],
 	files: [],
 };
