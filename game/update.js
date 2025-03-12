c7fd7959f8606d4eb381dc5c71eaf2589c30639e
NongJie <245722665@qq.com>
2025-03-12 13:02:23 +0800

    增加v1.0.4更新日志

--- game/update.js
+++ game/update.js
@@ -2,7 +2,9 @@ window.noname_update = {
 	version: "1.0.4",
 	update: "1.0.4", //新版本更新文件较多，直接强制进行全量更新
 	changeLog: [
-
+		'修复FAQ魔法入门无法正确触发Magic增加星石',
+		'优化魔法入门使用ai',
+		'修复多控响应技能无法进行选择触发',
 	],
 	files: [
 		'game/update.js',
