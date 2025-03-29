f6d0389f71ac1432de8a8fe7ea3fa99c3ebc16d6
NongJie <245722665@qq.com>
2025-03-29 11:19:13 +0800

    更新1.0.19

--- game/update.js
+++ game/update.js
@@ -1,16 +1,10 @@
 window.noname_update = {
-	version: "1.0.18",
-	update: "1.0.18", 
+	version: "1.0.19",
+	update: "1.0.19", 
 	changeLog: [
-		'checkMarks增加检查全局技能',
-		'修复移除部分指示物无日志',
-		'优化妖精秘法代码',
-		'优化启动ai',
-		'删除无用代码',
-		'完善精灵射手精灵秘术的无法行动及日志',
-		'修复黄金律bug',
-		'优化共振描述',
-		'优化贤者魔道法典ai',
+		'增加canTeShu玩家函数',
+		'完善canXingDong逻辑',
+		'完善爆牌时机',
 	],
 	files: [
 
