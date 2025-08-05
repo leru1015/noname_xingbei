22ef5b9d31a13190787db60213a115836fe6f815
NongJie <245722665@qq.com>
2025-08-05 15:04:12 +0800

    修复报错

--- noname/get/index.js
+++ noname/get/index.js
@@ -13,7 +13,6 @@ import security from "../util/security.js";
 import { CodeSnippet, ErrorManager } from "../util/error.js";
 
 import { GetCompatible } from "./compatible.js";
-import { listenerCount } from "process";
 
 export class Get extends GetCompatible {
 	is = new Is();
